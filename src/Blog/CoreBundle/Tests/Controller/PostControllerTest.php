846fc61ac149d3be2b3b3ed39ae3676b4b1307f7
szabolaszlo <szabolaszlo@shoprenter.hu>
2017-08-17 12:52:00 +0200

    HW 3 / part 4 (Fix tests)

--- src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -13,11 +13,8 @@ class PostControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
-
-        $this->assertTrue($client->getResponse()->isSuccessful(), 'Not Success');
-
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
         $this->assertCount(3, $crawler->filter('h2'), 'There should be 3 displayed posts');
     }
 
@@ -29,11 +26,10 @@ class PostControllerTest extends WebTestCase
         $client = static::createClient();
         /** @var Post $post */
         $post = $client->getContainer()->get('doctrine')->getManager()->getRepository('ModelBundle:Post')->findFirst();
-        $crawler = $client->request('GET', '/en/'.$post->getSlug());
+        $crawler = $client->request('GET', '/'.$post->getSlug());
         $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
         $this->assertEquals($post->getTitle(), $crawler->filter('h1')->text(), 'Invalid post title');
-        $this->assertGreaterThanOrEqual(1, $crawler->filter('article.comment')->count(), 'There should be at least 1 comment');
-    }
+        $this->assertGreaterThanOrEqual(1, $crawler->filter('article.comment')->count(), 'There should be at least 1 comment');    }
 
     /**
      * Test create a comment
@@ -47,16 +43,17 @@ class PostControllerTest extends WebTestCase
         $buttonCrawlerNode = $crawler->selectButton('Send');
         $form = $buttonCrawlerNode->form(array(
             'comment[authorName]' => 'A humble commenter',
-            'comment[body]' => "Hi! I'm commenting about Symfony2!"
+            'comment[body]' => "Hi! I'm commenting about Symfony2!",
         ));
         $client->submit($form);
         $this->assertTrue(
-            $client->getResponse()->isRedirect('/'.$post->getSlug()), 'There was no redirection after submitting the form'
+            $client->getResponse()->isRedirect('/'.$post->getSlug()),
+            'There was no redirection after submitting the form'
         );
         $crawler = $client->followRedirect();
         $this->assertCount(
             1,
-            $crawler->filter('html:contains("Your comment was submitted successfuly")'),
+            $crawler->filter('html:contains("Your comment was submitted successfully")'),
             'There was not any confirmation message'
         );
     }
--- src/Blog/CoreBundle/Tests/Controller/TagControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/TagControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Blog\CoreBundle\Tests\Controller;
 
+use Blog\ModelBundle\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TagControllerTest extends WebTestCase
@@ -9,8 +10,13 @@ class TagControllerTest extends WebTestCase
     public function testShow()
     {
         $client = static::createClient();
-
-        $crawler = $client->request('GET', '/show');
+        /** @var Tag $tag */
+        $tags = $client->getContainer()->get('doctrine')->getManager()->getRepository('ModelBundle:Tag')->findUsedTags();
+        $tag = $tags[0];
+        $tagPostsCount = $tag->getPosts()->count();
+        $crawler = $client->request('GET', '/tag/'.$tag->getSlug());
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
+        $this->assertCount($tagPostsCount, $crawler->filter('h2'), 'There should be '.$tagPostsCount.' posts');
     }
 
 }
